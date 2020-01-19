745818be2790a5117d64d40cfe572b8c7e538de4
Ha Young Jin <teen14y@gmail.com>
2020-01-19 15:11:15 +0900

    to save image data in editorstate

--- src/sequelize/models/post.js
+++ src/sequelize/models/post.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     "Post",
     {
       editorContentState: DataTypes.TEXT,
-      editorTitleState: DataTypes.TEXT
+      editorTitleState: DataTypes.LONGTEXT
     },
     {}
   );
