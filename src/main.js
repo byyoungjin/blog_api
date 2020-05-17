da20f6babc72c78f38693684ad5e2d205c0423ac
Ha Young Jin <teen14y@gmail.com>
2020-05-18 07:22:47 +0900

    cors

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import config from "@/config";
 import cors from "cors";
 
 const corsOptions = {
-  origin: ["https://hyjpost.com", /localhost:*/],
+  origin: [/https:\/\/hyjpost\.com.*/, /localhost:*/],
   methods: ["GET", "POST", "PUT", "DELETE"],
   preflightContinue: true
 };
@@ -15,7 +15,7 @@ const corsOptions = {
 const app = express();
 
 app.use(logger("dev"));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
